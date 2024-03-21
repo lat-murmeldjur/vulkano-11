91d584044b96a5159df5ef19c4af5c4796ecf250
lat-murmeldjur <latmurmeldjur@gmail.com>
2024-03-21 14:51:35 +0100

    "Auto commit number 27"

--- src/anomaly.rs
+++ src/anomaly.rs
@@ -6,7 +6,7 @@ pub struct Anomaly {
 
 pub struct Composition {
     pub Space: Vec<[f32; 3]>,
-    pub Distribution: Vec<fn([f32; 3])>,
+    pub Distribution: Vec<fn(Vec<[f32; 3]>) -> Vec<[f32; 3]>>,
 }
 
 pub struct Component {
@@ -22,6 +22,66 @@ pub struct Property {
 
 pub struct Force {
     pub Force: Vec<Force>,
+    pub Relative: Vec<f64>,
     pub Range: Vec<f64>,
     pub Domain: Vec<Component>,
+}
+
+pub fn particle(position: [f32; 3], properties: Vec<Property>) -> Anomaly {
+    let mut anom = Anomaly {
+        Anomaly: vec![],
+        Component: vec![Component {
+            Component: vec![],
+            Composition: vec![Composition {
+                Space: vec![position],
+                Distribution: vec![particular],
+            }],
+            Property: properties,
+        }],
+        Force: force().Force,
+    };
+
+    anom
+}
+
+pub fn particular(coordinates: Vec<[f32; 3]>) -> Vec<[f32; 3]> {
+    return coordinates;
+}
+
+pub fn force() -> Force {
+    return Force {
+        Force: vec![
+            // S
+            Force {
+                Force: vec![],
+                Relative: vec![1.0],
+                Range: vec![],
+                Domain: vec![],
+            },
+            // EM
+            Force {
+                Force: vec![],
+                Relative: vec![1.0 / 137.0],
+                Range: vec![f64::MAX],
+                Domain: vec![],
+            },
+            // W
+            Force {
+                Force: vec![],
+                Relative: vec![1e-6],
+                Range: vec![],
+                Domain: vec![],
+            },
+            // G
+            Force {
+                Force: vec![],
+                Relative: vec![1e-41],
+                Range: vec![f64::MAX],
+                Domain: vec![],
+            },
+        ],
+        Relative: vec![],
+        Range: vec![],
+        Domain: vec![],
+    };
 }
